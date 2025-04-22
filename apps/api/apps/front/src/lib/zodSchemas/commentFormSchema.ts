aa5fdc875bac241f4186f1cc587e26930d4b60eb
mohammad rida <mhmd_rida_990@hotmail.com>
2025-04-22 16:25:03 +0300

    d

--- apps/api/apps/front/src/lib/zodSchemas/commentFormSchema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const CommentFormSchema = z.object({
-  content: z.string().min(5),
-  postId: z
-    .string()
-    .transform((val) => parseInt(val))
-    .refine((val) => !isNaN(val)),
-});
